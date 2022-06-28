74240007ebcc6b4ec90d8bd548bf20597a297936
mesut <yildirimesutcw@gmail.com>
2022-06-28 21:28:19 +0300

    models

--- quiz/models.py
+++ quiz/models.py
@@ -2,41 +2,37 @@ from django.db import models
 
 # Create your models here.
 
-
 class Category(models.Model):
     name = models.CharField(max_length=20)
     
     def __str__(self):
         return self.name 
-
 class Quiz(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
-    date_created = models.DateTimeField(auto_now_add=True)
-
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
     def __str__(self):
         return self.title
 
-       
-
 class Question(models.Model):
-    # quiz = models.ForeignKey(Quiz, related_name='question', on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
+    quiz =  models.ForeignKey(Quiz, on_delete=models.CASCADE)
     DIFFICULTY = (
        ('H', 'High'),
        ('M', 'Medium'),
        ('L', 'Low'),
     )
     difficulty = models.CharField(max_length=5, choices=DIFFICULTY, default='L')
-    date_created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.title 
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, related_name='answer', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=30)
     is_right = models.BooleanField(default=False)
-    updated = models.DateTimeField(auto_now=True)   
+    updated = models.DateTimeField(auto_now=True, null=True)  
